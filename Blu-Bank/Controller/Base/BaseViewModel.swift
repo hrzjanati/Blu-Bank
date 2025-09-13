87c37647d9ed045bdf8dcd1cf0ab160b1b37ff1c
Nik <hrzjanati@gmail.com>
2025-09-13 16:09:18 +0330

    Making the project structure

---
+++ Blu-Bank/Controller/Base/BaseViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  BaseViewModel.swift
+//  Blu-Bank
+//
+//  Created by Nik on 13/09/2025.
+//
+
+import Foundation
+
+class BaseViewModel : ObservableObject {
+    
+    // MARK: - ----------------- Init
+    init() {
+        
+    }
+}
--- Blu-Bank/Controller/UI/Home/HomeViewController.swift
+++ Blu-Bank/Controller/UI/Home/HomeViewController.swift
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: BaseViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
